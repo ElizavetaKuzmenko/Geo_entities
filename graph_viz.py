019db118a791d2b1ff1f63995456f49e6d69bff6
Elizaveta Kuzmenko <lizaku77@gmail.com>
2016-06-24 23:58:06 +0300

    new pictures

--- graph_viz.py
+++ graph_viz.py
@@ -3,16 +3,16 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.read_gexf('cooccurrences_cities.gexf')
+G = nx.read_gexf('cooccurrences_countries.gexf')
 nodes = set()
 for (u, v, c) in G.edges(data='century'):
     if c == 'XIX':
         nodes.add(u)
         nodes.add(v)
         #print('(%s, %s, %s)' % (u, v, c))
-h = G.subgraph(nodes)
+#h = G.subgraph(nodes)
 
-nx.draw(h, node_size=30)
+nx.draw(G, node_size=30)
 #nx.draw_networkx_labels(G,pos=nx.spring_layout(G),labels=G.nodes())
-plt.show()
-plt.savefig("cities_XIX.png")
+#plt.show()
+plt.savefig("cooccurrences_countries.png")
