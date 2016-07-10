f02286e7d215bbd4aa2caf0f80a13f45494850cf
lizaku55 <lizaku77@gmail.com>
2016-07-10 18:06:19 +0300

    poster a,ong other things

--- make_graph.py
+++ make_graph.py
@@ -2,6 +2,10 @@
 
 import json, codecs, itertools
 
+with open('eng_rus_countries.json') as j:
+    map = json.loads(j.read())
+    eng_names = dict((v,k) for k, v in map.iteritems())
+
 PATH_DATA = u'/home/liza/Документы/geo/'
 output_f = codecs.open('cooccurrences_countries.gexf', 'w')
 output_f.write('<?xml version="1.0" encoding="UTF-8"?>\n<gexf xmlns="http://www.gexf.net/1.2draft" xmlns:xsi="' +\
@@ -11,7 +15,8 @@ output_f.write('<?xml version="1.0" encoding="UTF-8"?>\n<gexf xmlns="http://www.
 output_f.write('<graph defaultedgetype="undirected" label="co-occurrences graph for cities and countries" mode="static">')
 output_f.write('<attributes class="edge" mode="static">\n' +
                '<attribute id="0" title="author" type="string"/>\n' +\
-               '<attribute id="1" title="century" type="string"/>\n</attributes>\n<nodes>')
+               '<attribute id="1" title="century" type="string"/>\n' +\
+               '<attribute id="2" title="decade" type="string"/>\n</attributes>\n<nodes>')
 cities_id = {}
 countries_id = {}
 edges_id = {}
@@ -22,7 +27,7 @@ d = []
 cities = []
 countries = []
 
-with open(PATH_DATA + u'file_base.json', 'r') as f:
+with open('file_base.json', 'r') as f:
     data = json.loads(f.read())
     n = 0
     for piece in data:
@@ -38,7 +43,8 @@ with open(PATH_DATA + u'file_base.json', 'r') as f:
 
         author = data[piece]['author']
         century = data[piece]['century']
-        d.append([countries_together, author, century])
+        decade = data[piece]['decade']
+        d.append([countries_together, author, century, decade])
 
 print(len(countries), len(d))
 
@@ -54,8 +60,11 @@ for country in set(countries):
     if country not in countries_id:
         countries_id[country] = co_id
         co_id += 1
-    s = '<node id="%s" label="%s">\n<attvalues/>\n</node>' % (country, country) # str(countries_id[country]))
-    output_f.write(s.encode('utf-8'))
+    try:
+        s = '<node id="%s" label="%s">\n<attvalues/>\n</node>' % (eng_names[country], eng_names[country]) # str(countries_id[country]))
+        output_f.write(s.encode('utf-8'))
+    except KeyError:
+        continue
 output_f.write('</nodes>\n<edges>')
 
 for piece in d:
@@ -70,11 +79,14 @@ for piece in d:
         #    e_id = ed_id
         #    ed_id += 1
         #print pair[0], pair[1]
-        s = '<edge id="%s" source="%s" target="%s">' % (str(ed_id), pair[0], pair[1]) #str(countries_id[pair[0]]), str(countries_id[pair[1]]))
-        s += '<attvalues>\n<attvalue for="0" value="%s"/>\n<attvalue for="1" value="%s"/>\n</attvalues>\n</edge>' \
-             % (piece[1], piece[2])
-        ed_id += 1
-        output_f.write(s.encode('utf-8'))
+        try:
+            s = '<edge id="%s" source="%s" target="%s">' % (str(ed_id), eng_names[pair[0]], eng_names[pair[1]]) #str(countries_id[pair[0]]), str(countries_id[pair[1]]))
+            s += '<attvalues>\n<attvalue for="0" value="%s"/>\n<attvalue for="1" value="%s"/>\n<attvalue for="2" value="%s"/>\n</attvalues>\n</edge>' \
+             % (piece[1], piece[2], piece[3])
+            ed_id += 1
+            output_f.write(s.encode('utf-8'))
+        except KeyError:
+            continue
 
 
 output_f.write('</edges>\n</graph>\n</gexf>')
---
+++ toponyms_ipm.py
@@ -0,0 +1,6 @@
+__author__ = 'lizaku55'
+
+import json
+
+with open('file_base.json') as f:
+    d = json.loads(f.read())
